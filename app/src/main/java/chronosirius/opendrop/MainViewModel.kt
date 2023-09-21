0db7960fef0e83aeada7b5f5b5157e116ecd7e91
chronosirius <73508925+chronosirius@users.noreply.github.com>
2023-09-20 18:59:20 -0700

    e

--- app/src/main/java/chronosirius/opendrop/MainViewModel.kt
+++ app/src/main/java/chronosirius/opendrop/MainViewModel.kt
@@ -30,11 +30,31 @@ class MainViewModel: ViewModel() {
     fun loadStuff() {
         viewModelScope.launch {
             _state.value = _state.value.copy(isLoading = true)
-            delay(3000L)
+            val cache = DiskBasedCache(cacheDir, 1024*1024)
+            val network = BasicNetwork(HurlStack())
+            val requestQueue = RequestQueue(cache, network).apply {
+                start()
+            }
+            val filesJsonArrayRequest = JsonArrayRequest(Request.Method.GET, "https://192.168.1.1", null, 
+                Response.Listener {response ->
+                    _state.value = _state.value.copy(
+                        isLoading = false,
+                        files = this.processJson(response)
+                    )
+                }
+            )
             _state.value = _state.value.copy(
                 isLoading = false,
                 files = _state.value.files.toMutableList().also { it.add(File("test", 1024)) }
             )
         }
+    }
+
+    fun processJson(array: List<Map>) {
+        val j: MutableList<File> = MutableCollection()
+        for (o in array) {
+            j.add(File(o.name, o.size))
+        }
+        return j
     }
 }
